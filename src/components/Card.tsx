1a36225bebf1dbffa04733bea2034de96af3eb28
Galdrial <camerano.simo@gmail.com>
2025-12-05 17:54:17 +0100

    fix: correct anchor tag placement in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,15 @@
 
 function Card({src, alt, title, description, span }: {src?: string, alt?: string, title?: string, description?: string, span?: string}) { 
   return (
+    
     <div className={`${span} bg-green-500 p-6 rounded-lg flex flex-col items-center text-center`}>
+      <a href="/">
       <img src={src} alt={alt} className='w-32' />
       <h2 className='text-green-950 text-2xl font-bold mt-4 mb-2'>{title}</h2>
       <p className='text-green-900'>{description} </p>
+      </a>
     </div>
+    
   );
 }
 
