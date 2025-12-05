c91b76a12652fbfdf5d96ed92e33b2a5e1af96ab
Galdrial <camerano.simo@gmail.com>
2025-12-05 18:41:37 +0100

    fix: adjust image width in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ function Card({src, alt, title, description, span }: {src?: string, alt?: string
     
     <div className={`${span} bg-green-500 p-6 rounded-lg flex flex-col items-center text-center`}>
       <a href="/">
-      <img src={src} alt={alt} className='w-32' />
+      <img src={src} alt={alt} className='w-16' />
       <h2 className='text-green-950 text-2xl font-bold mt-4 mb-2'>{title}</h2>
       <p className='text-green-900'>{description} </p>
       </a>
